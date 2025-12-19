3ac6b05182085fb6b693d9b3da3ce6fe586ade3c
bizhong <bizhong@yahoo.com>
2025-12-18 18:03:08 -0600

    initial commit

---
+++ content/Notes/Learning-Hugo.md
@@ -0,0 +1,29 @@
++++
+date = '2025-12-18T15:42:39-06:00'
+draft = true
+title = 'Learning Hugo'
++++
+
+## Install Hugo on Raspberry Pi:
+    1. sudo apt update
+    2. sudo apt install snapd
+
+## Create First Hugo Site:
+1. Create a new site with following command:  
+    **hugo new site Hugosite**
+2. Make the newly created Hugosite your current working directory:  
+    **cd Hugosite**
+3. Create a git repository at the Hugosite root directory:  
+    **git init**
+4. Create a hugo theme using submodule command:  
+    **git submodule add https://github.com/McShelby/hugo-theme-relearn.git themes/hugo-theme-relearn**  
+5. Add the following line at the end of hugo.toml file:  
+    *theme = 'hugo-theme-relearn'*  
+6. Add one page into the new site:  
+    **hugo new content Notes/Learning-Hugo.md**  
+    then add some content into the content/NotesLearning-Hugo.md file.
+7. Test the newly created site:  
+    **hugo server -D**
+8. Initial result:  
+    point your browser at [http://localhost:1313](http://localhost:1313), the new page should show up.
+
---
+++ public/searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Path to my memory \u003e Notes",
+    "content": "Install Hugo on Raspberry Pi: 1. sudo apt update Create First Hugo Site: 1. Create a new site with following command: hugo new site Hugosite 2. Make the newly created Hugosite your current working directory: cd Hugosite 3. Create a git repository at the Hugosite root directory: git init 4. Create a hugo theme using submodule command: git submodule add https://github.com/McShelby/hugo-theme-relearn.git themes/hugo-theme-relearn 5. Add the following at the end of hugo.toml file at the root level of Hugosite directory: theme = 'hugo-theme-relearn'",
+    "description": "Install Hugo on Raspberry Pi: 1. sudo apt update Create First Hugo Site: 1. Create a new site with following command: hugo new site Hugosite 2. Make the newly created Hugosite your current working directory: cd Hugosite 3. Create a git repository at the Hugosite root directory: git init 4. Create a hugo theme using submodule command: git submodule add https://github.com/McShelby/hugo-theme-relearn.git themes/hugo-theme-relearn 5. Add the following at the end of hugo.toml file at the root level of Hugosite directory: theme = 'hugo-theme-relearn'",
+    "tags": [],
+    "title": "Learning Hugo",
+    "uri": "/notes/learning-hugo/index.html"
+  },
+  {
+    "breadcrumb": "Path to my memory",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Notes",
+    "uri": "/notes/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Path to my memory",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Path to my memory",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Path to my memory",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
